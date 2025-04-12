4131dfa1ce3c51b5a46b2fff37c8a8f19f432292
Jennifer Tongco <jennifer.j.tongco@gmail.com>
2025-04-12 09:33:56 +0800

    create database

--- website/__init__.py
+++ website/__init__.py
@@ -9,31 +9,34 @@ from dotenv import load_dotenv, find_dotenv
 
 
 def create_app():
-    load_dotenv(find_dotenv())  # <- this makes sure .env is found no matter what
+    load_dotenv(find_dotenv()) 
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev-secret")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "postgresql://paddy_rice_tracker_db_user:2RX9usq562ns4fB04L8Y2tsJAVD1YTaV@dpg-cvrqgq6r433s73b1sbng-a/paddy_rice_tracker_db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    print("✅ Loaded DB URI:", app.config['SQLALCHEMY_DATABASE_URI'])
-    # ✅ Initialize Extensions
+    print(" Loaded DB URI:", app.config['SQLALCHEMY_DATABASE_URI'])
+    # Initialize Extensions
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.login_view = 'auth.login'
     login_manager.login_message = ''
     login_manager.init_app(app)
 
-    # ✅ Register Blueprints
+    # Register Blueprints
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(api, url_prefix="/api")
     app.register_blueprint(google_bp, url_prefix="/login")
     app.register_blueprint(views, url_prefix="/")
 
-    # ✅ Import models after db is initialized!
+    #Import models after db is initialized!
     with app.app_context():
-        from .models import User, Farmer, DryingRecord
+        from .models import User, Farmer, DryingRecord, Municipality, Barangay
+        
+        # Create all tables
+        db.create_all()
 
-    # ✅ Login user loader
+    # Login user loader
     @login_manager.user_loader
     def load_user(user_id_str):
         from .models import User, Farmer # Import both models
